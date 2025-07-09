c444512253f39655fd8b3a99a6c24bdd749bb2ea
KingCrimsonJY <110095263+KingCrimsonJY@users.noreply.github.com>
2025-07-09 16:16:56 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -2,6 +2,5 @@ rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
-git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
 git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
