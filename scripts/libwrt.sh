fe658378b505d1034d872446ed7e1a0002007eeb
KingCrimsonJY <kingcrimsonjy@163.com>
2025-07-19 18:22:54 +0800

    openlist2

--- scripts/libwrt.sh
+++ scripts/libwrt.sh
@@ -12,5 +12,5 @@ git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
 git clone --depth=1 https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
-git clone --depth=1 https://github.com/sbwml/luci-app-openlist.git package/openlist
+git clone --depth=1 https://github.com/sbwml/luci-app-openlist2.git package/openlist
 git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
