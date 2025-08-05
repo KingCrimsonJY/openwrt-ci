7ad144eafea0707225e5cf499bbec35f6bbeb83c
KingCrimsonJY <kingcrimsonjy@163.com>
2025-08-06 03:36:16 +0800

    更新插件

--- scripts/libwrt.sh
+++ scripts/libwrt.sh
@@ -6,6 +6,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 rm -rf feeds/packages/net/open-app-filter
 rm -rf feeds/packages/net/openlist
 rm -rf feeds/luci/applications/luci-app-openlist
+rm -rf feeds/packages/net/openlist2
+rm -rf feeds/luci/applications/luci-app-openlist2
 rm -rf feeds/luci/applications/luci-app-openclash
 
 rm -rf package/emortal/luci-app-athena-led
@@ -13,6 +15,6 @@ git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
 git clone --depth=1 https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
-git clone --depth=1 https://github.com/sbwml/luci-app-openlist2.git package/openlist
+git clone --depth=1 https://github.com/sbwml/luci-app-openlist2.git package/openlist2
 git clone --depth=1 https://github.com/vernesong/OpenClash.git package/openclash
 git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
